ab59d3e387d3429477347a701d45a65acb7ccf55
nivedha-duraivel <nivedhad20001@gmail.com>
2021-09-23 19:15:28 +0530

    Spring file added

---
+++ Management_account/src/main/java/com/spot/ManagementAccountApplication.java
@@ -0,0 +1,13 @@
+package com.spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementAccountApplication.class, args);
+	}
+
+}
